54d73e07da7cab56d06e258c9e23711a265c1188
Taniya Nawal Pathak <112875959+tansexe@users.noreply.github.com>
2024-09-05 02:24:37 +0530

    Kuch toh bana h

--- app/Pages/Dashboard/page.tsx
+++ app/Pages/Dashboard/page.tsx
@@ -1,7 +1,3 @@
 export default function Dashboard() {
-  return (
-    <div>
-      <h1>Welcome to Dashboard Page</h1>
-    </div>
-  );
+  return <div>Welcome to Dashboard Page</div>;
 }
--- app/Pages/ui/Dashboard/sidebar/sidebar.tsx
+++ app/Pages/ui/Dashboard/sidebar/sidebar.tsx
@@ -1,7 +1,62 @@
-export default function sidebar() {
+import styles from "./sidebar.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import { MdDashboard, MdGroup, MdLogout } from "react-icons/md";
+
+const user = {
+  img: "/noavatar.png",
+  username: "John Doe",
+};
+
+const menuItems = [
+  {
+    list: [
+      {
+        title: "Dashboard",
+        path: "/Pages/Dashboard",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Attendance",
+        path: "/Pages/Dashboard/Attendance",
+        icon: <MdGroup />,
+      },
+    ],
+  },
+];
+
+const MenuLink = ({ item }) => {
   return (
-    <div>
-      <h1>Sidebar</h1>
+    <Link href={item.path} className={styles.menuLink}>
+      {item.icon}
+      <span>{item.title}</span> {/* Add back the text span */}
+    </Link>
+  );
+};
+
+export default function Sidebar() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.user}>
+        <Image
+          className={styles.userImage}
+          src={user.img || "/noavatar.png"}
+          alt="User Avatar"
+          width="50"
+          height="50"
+        />
+        <div className={styles.userDetail}>
+          <span className={styles.username}>{user.username}</span>
+          <span className={styles.userTitle}>Administrator</span>
+        </div>
+      </div>
+      <ul className={styles.menuList}>
+        {menuItems[0].list.map((item, index) => (
+          <li key={index} className={styles.menuItem}>
+            <MenuLink item={item} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
