4d9af3572097b64f4e88f009eb36c2dcb15c0b48
Aarush Sen <senaarush@outlook.com>
2024-09-05 12:20:31 +0530

    feat: add user model with required fields and default values

---
+++ app/models/user.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+
+    roll: {
+        type: Number,
+        required: true
+    },
+
+    branch: {
+        type: String,
+        required: true
+    },
+
+    domain: {
+        type: String,
+        required: true
+    },
+
+    role: {
+        type: [String],
+        required: true
+    },
+    
+    archived: {
+        type: Boolean,
+        default: false
+    },
+
+})
+
+const userModel = mongoose.model("User", userSchema)
+
+export default userModel
