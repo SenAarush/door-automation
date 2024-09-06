dd10705e43c4f3d36f9ae267330b4f0d18be43fb
Aarush Sen <senaarush@outlook.com>
2024-09-06 08:42:33 +0530

    feat: Update user validation schema with email field

--- app/validators/user.ts
+++ app/validators/user.ts
@@ -1,12 +1,27 @@
 import { z } from "zod"
 
 const userZodSchema = z.object({
+
     name: z.string().min(1),
     roll: z.number().int().min(1),
+    email: z.string().email().min(1),
+    password: z.string().min(1),
+    cPassword: z.string().min(1).optional(),
     branch: z.string().min(1),
     domain: z.string().min(1),
     role: z.array(z.string()).min(1),
-    archived: z.boolean().default(false)
+    archived: z.boolean().optional()
+
 })
 
-export { userZodSchema }
+const loginZodSchema = z.object({
+
+    roll: z.number().int().min(1),
+    password: z.number().int().min(1)
+    
+})
+
+export { 
+    userZodSchema,
+    loginZodSchema
+}
