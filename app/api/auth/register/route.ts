15aa745e72f9343e388249d8af6503ea70747e1a
Aarush Sen <senaarush@outlook.com>
2024-09-06 08:42:23 +0530

    feat: Add registration route and logic with user validation schema and getUserByParam function

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,92 +1,127 @@
-import connectDB from "@/app/utils/mongoDB"
-import { NextRequest, NextResponse } from "next/server"
-import { userZodSchema } from "@/app/validators/user"
-import userModel from "@/app/models/user"
-import { generateToken } from "@/app/utils/jwt"
-import { z } from "zod"
+import connectDB from "@/app/utils/mongoDB";
+import { NextRequest, NextResponse } from "next/server";
+import { userZodSchema } from "@/app/validators/user";
+import userModel from "@/app/models/user";
+import { generateToken } from "@/app/utils/jwt";
+import { z } from "zod";
+import getUserByParam from "../../getUserByParam";
 
-type userType = z.infer<typeof userZodSchema>
+type userType = Omit<z.infer<typeof userZodSchema>, "cPassword">;
 
-async function getUserByRoll(
-    roll: number
-) {
-    try {
-        return await userModel.findOne({ roll: roll });
-    } catch (error) {
-        console.log(error)
-        throw new Error("Error finding user in DB");
-    }
-}
-
-async function saveUserToDB(
-    data: userType
-) {
+async function saveUserToDB(data: userType) {
     const newUser = new userModel({
         ...data,
     })
     try {
-        return await newUser.save();
+        return await newUser.save()
     } catch (error) {
         console.log(error)
-        throw new Error("Error saving user to DB");
+        throw new Error("Error saving user to DB")
     }
 }
 
-export async function POST(
-    req: NextRequest
-) {
-    await connectDB()
+export async function POST(req: NextRequest) {
+    try {
+        await connectDB();
 
-    const reqBody = await req.json()
-    const validatedReqBody = userZodSchema.safeParse(reqBody)
+        const reqBody = await req.json();
+        const validatedReqBody = userZodSchema.safeParse(reqBody);
 
-    if (!validatedReqBody.success) {
-        return NextResponse.json(
-            {
-                success: false,
-                message: validatedReqBody.error,
-            },
-            { status: 400 }
-        )
-    }
+        if (!validatedReqBody.success) {
+            return NextResponse.json(
+                {
+                    success: false,
+                    message: validatedReqBody.error.flatten(),
+                },
+                { status: 400 }
+            );
+        }
 
-    const {
-        name,
-        branch,
-        domain,
-        role,
-        roll
-    } = validatedReqBody.data
+        const {
+            name,
+            email,
+            password,
+            cPassword,
+            branch,
+            domain,
+            role,
+            roll
+        } = validatedReqBody.data
 
-    const existingUser = await getUserByRoll(roll)
-    if (existingUser) {
+        if (password !== cPassword) {
+            return NextResponse.json(
+                {
+                    success: false,
+                    message: "Passwords do not match",
+                },
+                { status: 400 }
+            )
+        }
+
+        if (Number(email.split("@")[0]) !== roll) {
+            return NextResponse.json(
+                {
+                    success: false,
+                    message: "Email and Roll number do not match",
+                },
+                { status: 400 }
+            )
+        }
+
+        const existingUser = await getUserByParam(roll, "roll")
+
+        if (existingUser) {
+            return NextResponse.json(
+                {
+                    success: false,
+                    message: "User already exists",
+                },
+                { status: 409 }
+            )
+        }
+
+        const createdUser = await saveUserToDB({
+            name,
+            email,
+            password,
+            branch,
+            domain,
+            role,
+            roll
+        })
+
+        const token = generateToken(createdUser.roll, role)
+
+        if (!token) {
+            return NextResponse.json(
+                {
+                    success: false,
+                    message: "JWT Secret not defined",
+                },
+                { status: 500 }
+            )
+        }
+
         return NextResponse.json(
             {
                 success: true,
-                message: "User already exists",
+                message: "User created successfully",
+                user: createdUser,
+                token: token,
             },
-            { status: 200 }
+            { status: 201 }
+        )
+
+    } catch (error: any) {
+
+        console.log("Error creating user", error)
+
+        return NextResponse.json(
+            {
+                success: false,
+                message: error.message,
+            },
+            { status: 500 }
         )
     }
-
-    const createdUser = await saveUserToDB({
-        name,
-        branch,
-        domain,
-        role,
-        roll,
-    })
-
-    const token = generateToken(createdUser.roll, role);
-
-    return NextResponse.json(
-        {
-            success: true,
-            message: "User created successfully",
-            user: createdUser,
-            token: token,
-        },
-        { status: 201 }
-    )
-
 }
